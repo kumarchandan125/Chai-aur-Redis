ed18181fa77b28bf414498f3eea5bd75699e62d6
kumarchandan125 <chandanpandey6048178@gmail.com>
2026-05-28 13:04:02 +0530

    This is my journey of redis

---
+++ 02-Setup-local-redis/src/index.js
@@ -0,0 +1,26 @@
+import express from "express";
+import mongoose from "mongoose";
+import Redis from "ioredis";
+
+const app = express();
+const redis = new Redis(process.env.REDIS_URL || "redis://localhost:6379");
+
+app.get("/redis", async (req, res) => {
+  const reply = await redis.ping();
+  res.send({ redis: reply });
+});
+
+app.get("/mongo", async (req, res) => {
+  const Url =
+    process.env.MONGO_URL || "mongodb://localhost:27017/chai_aur_redis";
+  if (mongoose.connection.readyState === 0) {
+    await mongoose.connect(Url);
+  }
+  res.send({ mongo: "connected", database: mongoose.connection.name });
+});
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port http://localhost:${PORT}`);
+});
